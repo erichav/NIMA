fe093db9bcaf432d272fa497fc33e3c0e4738e5c
Pablo <pabloastadeo@gmail.com>
2018-11-17 23:06:23 -0600

    ...

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_restful import Api
+# from flask_restful import Api
 
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app)
+    # api = Api(app)
 
     @app.route('/')
     def index():
