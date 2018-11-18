962fd516941495de37e1a02f1c0150a20f2f0632
Pablo <pabloastadeo@gmail.com>
2018-11-17 23:44:59 -0600

    works

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-# from app import create_app
-# app = create_app()
-#
-#
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'OK!'
+from nima import create_app
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
