6ec077d477797d5bf31d8e93a3b9a417d961598b
Pablo <pabloastadeo@gmail.com>
2018-11-19 02:07:55 -0600

    front homepage

--- nima/__init__.py
+++ nima/__init__.py
@@ -3,10 +3,10 @@ from flask_restful import Api
 
 
 def create_app():
-    app = Flask(__name__)
-    api = Api(app)
+    app = Flask(__name__,static_url_path='/nima/static')
+    # api = Api(app)
 
     @app.route('/')
-    def index():
-        return 'INDEX'
+    def root():
+        return app.send_static_file('index.html')
     return app
